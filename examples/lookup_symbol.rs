f5fb31480b63d5509a82e947b6089fea00d35260
Nikhil Marathe <nsm.nikhil@gmail.com>
2018-08-10 22:00:10 -0700

    try to demangle symbols, but it does not seem to work very well

--- examples/lookup_symbol.rs
+++ examples/lookup_symbol.rs
@@ -1,6 +1,8 @@
 /// Given an address and the location of breakpad generated symbols, tries to find the relevant
 /// symbol.
 extern crate breakpad_symbols;
+extern crate rustc_demangle;
+use self::rustc_demangle::demangle;
 
 use breakpad_symbols::SymbolFile;
 use std::path::Path;
@@ -20,6 +22,6 @@ fn main() {
     for addr in args {
         let ip = u64::from_str_radix(&addr, 16).expect("valid hex address");
         let sym = sym_file.find_nearest_public(ip).expect("found some symbol");
-        println!("Symbol: {:?}", sym);
+        println!("Symbol: {}", demangle(&sym.name));
     }
 }
