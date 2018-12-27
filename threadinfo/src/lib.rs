dd5b77e5e05e750183257a0cad8561f4433da371
Nikhil Marathe <nsm.nikhil@gmail.com>
2018-12-26 23:14:22 -0800

    Create workspace with threadinfo crate implementation for Linux

---
+++ threadinfo/src/lib.rs
@@ -0,0 +1,59 @@
+/// threadinfo is a library to retrieve OS threads and related information in a platform indepedent
+/// manner. Currently it provides:
+/// - An iterator over the current process' threads.
+/// threadinfo explicitly deals with OS threads, even if such threads may be programmed against
+/// using an abstraction like pthreads. This is because the functionality it provides/intends to
+/// provide, uses OS APIs that rely on those details. It uses the Windows Threads API, Mach and
+/// Linux tasks.
+
+#[cfg(target_os = "linux")]
+pub mod linux;
+#[cfg(target_os = "linux")]
+pub use self::linux::*;
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+    use std::sync::{Arc,Barrier, Mutex};
+    use super::{current_thread, thread_iterator};
+
+    #[test]
+    fn current_thread_test() {
+        current_thread().expect("thread");
+    }
+
+    #[test]
+    fn thread_iterator_one() {
+        let curr = current_thread().expect("current thread");
+        assert!(thread_iterator().expect("iterator").any(|it| it == curr));
+    }
+
+    #[test]
+    fn thread_iterator_multiple() {
+        let mut threads = Vec::with_capacity(10);
+        let ids = Arc::new(Mutex::new(HashSet::new()));
+        let ids_barrier = Arc::new(Barrier::new(threads.capacity() + 1));
+        let terminate_barrier = Arc::new(Barrier::new(threads.capacity() + 1));
+        for _i in 0..threads.capacity() {
+            let ids2 = ids.clone();
+            let ids_barrier2 = ids_barrier.clone();
+            let terminate_barrier2 = terminate_barrier.clone();
+            threads.push(std::thread::spawn(move || {
+                ids2.lock().unwrap().insert(current_thread().unwrap());
+                ids_barrier2.wait();
+                terminate_barrier2.wait();
+            }));
+        }
+
+        ids_barrier.wait();
+
+        let its: HashSet<_> = thread_iterator().unwrap().collect();
+        assert!(its.len() >= threads.capacity() + 1);
+        assert!(its.is_superset(&ids.lock().unwrap()));
+
+        terminate_barrier.wait();
+        for thread in threads {
+            thread.join().unwrap();
+        }
+    }
+}
---
+++ threadinfo/src/linux.rs
@@ -0,0 +1,36 @@
+extern crate libc;
+
+use libc::{pid_t, syscall, SYS_gettid};
+use std::io::Result;
+use std::iter::Iterator;
+
+#[derive(Eq, PartialEq, Debug, Hash)]
+pub struct Thread(pid_t);
+
+impl Thread {
+    pub fn is_current_thread(&self) -> bool {
+        self == &current_thread().expect("current thread should never fail")
+    }
+}
+
+/// Returns an object for the current thread.
+pub fn current_thread() -> Result<Thread> {
+    let tid = unsafe { syscall(SYS_gettid) as pid_t };
+    Ok(Thread(tid))
+}
+
+/// Returns an iterator over the current process' threads.
+///
+/// This function does not guarantee that the threads it returns are the complete and full set of
+/// threads in the process. Threads may be created that are not in the iterator, and threads
+/// returned from this iterator may have terminated.
+pub fn thread_iterator() -> Result<impl std::iter::Iterator<Item = Thread>> {
+    std::fs::read_dir("/proc/self/task").and_then(|entries| {
+        Ok(entries.map(|entry| {
+            let file = entry.unwrap().file_name();
+            // Probably faster to send the get_thread thing a direct string from `path()`.
+            let tid = file.to_str().unwrap().parse::<i32>().unwrap();
+            Thread(tid)
+        }))
+    })
+}
